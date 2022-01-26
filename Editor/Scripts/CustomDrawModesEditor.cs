eb10206933e672f0f25dda51a8e3c412235aa474
Krzysztof B <bziuk.krzysztof@gmail.com>
2022-01-26 15:32:09 +0100

    Init

---
+++ Editor/Scripts/CustomDrawModesEditor.cs
@@ -0,0 +1,61 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[InitializeOnLoad]
+public static class CustomDrawModesEditor
+{
+    private const string CATALOG_NAME = "CustomDrawMode/";
+
+    private static Shader[] shaders;
+    private static int lastIndex = -1;
+
+    static CustomDrawModesEditor()
+    {
+        shaders = new[]
+        {
+            Shader.Find(CATALOG_NAME + "Vertex Color"),
+            Shader.Find(CATALOG_NAME + "Vertex Color - Red"),
+            Shader.Find(CATALOG_NAME + "Vertex Color - Green"),
+            Shader.Find(CATALOG_NAME + "Vertex Color - Blue"),
+            Shader.Find(CATALOG_NAME + "Vertex Color - Alpha"),
+            Shader.Find(CATALOG_NAME + "UV0")
+        };
+
+        SceneView.ClearUserDefinedCameraModes();
+
+        for (int i = 0; i < shaders.Length; i++)
+            SceneView.AddCameraMode(GetRealName(shaders[i]), "Custom");
+
+        SceneView.duringSceneGui += Draw;
+    }
+
+    private static string GetRealName(Shader shader)
+    {
+        return shader.name.Remove(0, CATALOG_NAME.Length);
+    }
+
+    private static void Draw(SceneView sceneView)
+    {
+        if (lastIndex != -1 && GetRealName(shaders[lastIndex]) == sceneView.cameraMode.name)
+        {
+            sceneView.camera.RenderWithShader(shaders[lastIndex], "");
+            return;
+        }
+
+        for (int i = 0; i < shaders.Length; i++)
+        {
+            if (GetRealName(shaders[i]) == sceneView.cameraMode.name)
+            {
+                sceneView.camera.RenderWithShader(shaders[i], "");
+                lastIndex = i;
+                return;
+            }
+        }
+
+        if (lastIndex != -1)
+        {
+            lastIndex = -1;
+            sceneView.camera.ResetReplacementShader();
+        }
+    }
+}
---
+++ Editor/Shaders/Vertex Color.shader
@@ -0,0 +1,46 @@
+﻿Shader "CustomDrawMode/Vertex Color"
+{
+    SubShader
+    {
+        Tags { "Queue" = "Transparent" "RenderType" = "Transparent" }
+        
+        Pass
+        {
+
+            CGPROGRAM
+
+            #pragma vertex vert
+            #pragma fragment frag
+
+            #include "UnityCG.cginc"
+
+            struct appdata
+            {
+                float4 vertex : POSITION;
+                float4 color : COLOR;
+            };
+
+            struct v2f
+            {
+                float4 pos : SV_POSITION;
+                float4 color : COLOR;
+            };
+            
+            v2f vert(appdata v)
+            {
+                v2f o;
+                o.pos = UnityObjectToClipPos(v.vertex);
+                o.color = float4(v.color.xyz, 1);
+                return o;
+            }
+            
+            fixed4 frag(v2f i) : SV_Target
+            {
+                return i.color;
+            }
+
+            ENDCG
+
+        }
+    }
+}
